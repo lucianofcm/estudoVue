e6e1f87357d7c641948fb49c564a435ca21f8556
lucianofcm <lucweb@gmail.com>
2017-08-24 22:42:24 -0300

    sfsasdafdf

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 /* eslint-disable no-new */
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import Home from './components/Home.vue'
+import App from './App.vue'
 
-Vue.component('app-server-status', Home)
+export const serverBus = new Vue()
 
 new Vue({
-  render: h => h(App),
-  el: '#app'
+  el: '#app',
+  render: h => h(App)
 })
